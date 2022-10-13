3a2e36dc34ba60b820de0d0e5ab91e0b83d58056
keey7 <109679251+keey7@users.noreply.github.com>
2022-10-13 23:42:06 +0200

    Update Empleado.java

--- src/practicandoarraylist/Empleado.java
+++ src/practicandoarraylist/Empleado.java
@@ -1,15 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package practicandoarraylist;
 
 import java.util.Objects;
 
-/**
- *
- * @author Alumno Mañana
- */
+
 public class Empleado {
     private String dni;
     private String nomEmpleado;
